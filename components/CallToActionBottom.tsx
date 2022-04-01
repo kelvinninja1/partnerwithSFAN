458d996a32837ef40b1d0410032215b112781432
Kelvin Morrison <ikelvinstudios@gmail.com>
2022-04-01 13:48:02 +0000

    Added: minor fix for mobile view

--- components/CallToActionBottom.tsx
+++ components/CallToActionBottom.tsx
@@ -1,9 +1,9 @@
 export default function CallToActionBottom() {
   return (
-    <section className="text-gray-600 body-font bg-call-to-action">
+    <section className="text-gray-600 body-font bg-call-to-action flex-col-reverse md:flex-col">
       <div className="container mx-auto flex px-5 py-15 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font lg:text-6xl sm:text-4xl text-3xl mb-4 text-blue-300 font-medium title-font">
+          <h1 className="title-font lg:text-6xl sm:text-4xl text-3xl mt-10 md:mt-auto mb-4 text-blue-300 font-medium title-font">
             Partner for Progress
           </h1>
           <p className="mb-8 leading-relaxed text-blue-200">
