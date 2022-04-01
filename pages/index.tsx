b4ddeff457a991365c0a9964cbf47276908e0520
Kelvin Morrison <ikelvinstudios@gmail.com>
2022-04-01 12:54:59 +0000

    Added: Meta data, Favicons and Manifest links

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,39 @@ import Footer from "../components/Footer";
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta
+          name="description"
+          content="SFAN is unlocking the potential of Africa's young geniuses by helping them turn their passion into businesses and fulfilling careers."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="https://www.sfanonline.org/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="https://www.sfanonline.org/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="https://www.sfanonline.org/favicon-16x16.png"
+        />
+        <link
+          rel="manifest"
+          href="https://www.sfanonline.org/site.webmanifest"
+        />
+        <title>
+          SFAN - unlocking employment and entrepreneurship potential of African
+          youth
+        </title>
+      </Head>
       <Hero />
       <CallToAction />
       <PartnershipOffering />
